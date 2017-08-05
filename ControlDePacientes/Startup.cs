b5c41dfaf20b173affcc55364c4820cd032ff212
Amiusco <amifranciscocortez@hotmail.com>
2017-08-05 13:22:32 -0500

    ControlDePacientes

---
+++ ControlDePacientes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ControlDePacientes.Startup))]
+namespace ControlDePacientes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
